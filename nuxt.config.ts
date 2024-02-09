d863dec42d15eb8d4c132a7603b67d6ce63063e9
Keone Somers <Keone.somers@outlook.com>
2024-02-09 21:44:43 +0000

    meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Texitor: Dedicated LaTeX Editor",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Texitor is a free and modern LaTeX editor for Windows, macOS and Linux.",
+        },
+        { name: "author", content: "Texitor" },
+      ],
+    },
+  },
 });
