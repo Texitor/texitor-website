963f7109427cdca27863118bad14523ec3747710
Keone Somers <Keone.somers@outlook.com>
2024-02-12 17:03:08 +0000

    added nuxt img package and link to docs

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -64,7 +64,7 @@ const navigation = [
   { name: "Home", href: "#" },
   { name: "Features", href: "#features" },
   { name: "FAQ", href: "#faqs" },
-  // { name: "Guide", href: "#" },
+  { name: "Guide", href: "https://docs.texitor.com/" },
 ];
 
 const mobileMenuOpen = ref(false);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/image"],
   app: {
     head: {
       charset: "utf-8",
@@ -19,5 +19,8 @@ export default defineNuxtConfig({
         { name: "author", content: "Texitor" },
       ],
     },
+  },
+  image: {
+    format: ["webp"],
   },
 });
