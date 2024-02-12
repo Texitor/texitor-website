ea6201ed418d38eb69aa3f84ed47aa4f50ace1cf
Ali Shahwali <shahwali@kth.se>
2024-02-12 19:19:25 +0100

    Reverted image changes

--- pages/post-download.vue
+++ pages/post-download.vue
@@ -45,9 +45,9 @@ onMounted(() => {
             >
               Steps for downloading on Windows:
             </h2>
-            <NuxtImg src="/WinStep1.png" alt="Step 1" />
-            <NuxtImg src="/WinStep2.png" alt="Step 2" />
-            <NuxtImg src="/WinStep3.png" alt="Step 3" />
+            <img src="/WinStep1.png" alt="Step 1" />
+            <img src="/WinStep2.png" alt="Step 2" />
+            <img src="/WinStep3.png" alt="Step 3" />
           </div>
           <div
             v-else-if="platform === 'macOS'"
@@ -58,8 +58,8 @@ onMounted(() => {
             >
               Steps for downloading on MacOS:
             </h2>
-            <NuxtImg src="/MacStep1.png" alt="Step 1" />
-            <NuxtImg src="/MacStep2.png" alt="Step 2" />
+            <img src="/MacStep1.png" alt="Step 1" />
+            <img src="/MacStep2.png" alt="Step 2" />
           </div>
         </ClientOnly>
 
