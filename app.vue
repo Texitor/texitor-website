3eebbc1319a3cddfe63f98c8c09aab8eefa8f03a
Keone Somers <Keone.somers@outlook.com>
2024-02-12 23:10:47 +0000

    added cloudflare analytics

--- app.vue
+++ app.vue
@@ -4,6 +4,14 @@
   </div>
 </template>
 
+<!-- Cloudflare Web Analytics -->
+<script
+  defer
+  src="https://static.cloudflareinsights.com/beacon.min.js"
+  data-cf-beacon='{"token": "5177a72985c846338aad3014ec801418"}'
+></script>
+<!-- End Cloudflare Web Analytics -->
+
 <style>
 html {
   scroll-behavior: smooth;
